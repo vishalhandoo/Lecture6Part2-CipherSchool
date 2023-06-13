b44fae6420fc9229d70c1218568b6c9d420685ae
vishalhandoo <81742803+vishalhandoo@users.noreply.github.com>
2023-06-13 17:55:38 +0530

    Add files via upload

---
+++ Lecture6part2.cpp
@@ -0,0 +1,22 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+    
+    int i = 1;
+    // for(i = 1 ; ;++i){ infinite loop
+    //     cout<<"Hello";
+    // }
+    // post increment and pre increment.
+    i=0;
+    int j=0;
+    int k=0;
+
+    //print number from one to 100
+
+    for(int i = 1 ; i <= 100 ; i++){
+        cout<<i<<" ";
+    }
+
+    return 0;
+}
